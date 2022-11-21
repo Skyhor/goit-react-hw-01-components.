d0f6492bd1f5c191d876a83d4cb575eef76a18bc
Yehor Skibnevskyi <105387497+Skyhor@users.noreply.github.com>
2022-11-21 07:00:37 +0100

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,10 @@
 // import friends from '../data/friends.json';
 import { Profile } from './User/User';
 import user from '../data/user.json';
+import { Statistics } from './Stats/Stats';
+import data from '../data/data.json';
+import { FriendList } from './Friend/Friend';
+import friends from '../data/friends.json';
 // import user from 'path/to/user.json';
 
 export const App = () => {
@@ -18,8 +22,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      {/* <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} /> */}
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
     </>
   );
 };
