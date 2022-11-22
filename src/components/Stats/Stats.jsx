0d38628822da9405c38784cf04d0e8dad280ebce
Yehor Skibnevskyi <105387497+Skyhor@users.noreply.github.com>
2022-11-22 13:13:27 +0100

    start

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -1,11 +1,28 @@
 import s from './Stats.module.css';
+const createColor = () => {
+  const color =
+    'rgba(' +
+    Math.round(Math.random() * 255) +
+    ',' +
+    Math.round(Math.random() * 255) +
+    ',' +
+    Math.round(Math.random() * 255) +
+    ',' +
+    0.5 +
+    ')';
+  return color;
+};
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={s.statistics}>
       {title && <h2 className={s.title}>{title}</h2>}
       <ul className={s.statList}>
         {stats.map(({ id, label, percentage }) => (
-          <li key={id} className={s.item}>
+          <li
+            key={id}
+            className={s.item}
+            style={{ backgroundColor: createColor() }}
+          >
             <span className={s.label}>{label}</span>
             <span className={s.percentage}>{percentage}%</span>
           </li>
