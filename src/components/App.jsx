ef30d227a341d9f90aa9bf253d19e6521555d271
Yehor Skibnevskyi <105387497+Skyhor@users.noreply.github.com>
2022-11-22 01:01:40 +0100

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,24 @@
-import { Profile } from './User/User';
-import user from '../data/user.json';
-import { Statistics } from './Stats/Stats';
-import data from '../data/data.json';
-import { FriendList } from './Friend/Friend';
-import friends from '../data/friends.json';
-import transactions from '../data/transactions.json';
-import { Transactions } from './TransactionHistory/TransactionHistory';
-export const App = () => {
-  return (
-    <>
-      <Profile
-        name={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
-      <Transactions transactions={transactions} />
-    </>
-  );
-};
-
 // import { Profile } from './User/User';
 // import user from '../data/user.json';
-// import { Statistics } from './Statics/Statics';
+// import { Statistics } from './Stats/Stats';
 // import data from '../data/data.json';
 // import { FriendList } from './Friend/Friend';
 // import friends from '../data/friends.json';
-
-// // import user from 'path/to/user.json';
+// import transactions from '../data/transactions.json';
+// import { Transactions } from './TransactionHistory/TransactionHistory';
+// export const App = () => {
+//   return (
+//     <>
+//       <Profile
+//         name={user.username}
+//         tag={user.tag}
+//         location={user.location}
+//         avatar={user.avatar}
+//         stats={user.stats}
+//       />
+//       <Statistics title="Upload stats" stats={data} />
+//       <FriendList friends={friends} />
+//       <Transactions transactions={transactions} />
+//     </>
+//   );
+// };
